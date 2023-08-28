2e37b89677175787a0abe34d874265b71301b5a1
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2023-08-29 01:18:37 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+function signIn(){
+    alert("\u26A0\ufe0f\u26A0\ufe0f\uD83D\uDE02\uD83D\uDEAB You Want to Watch movies!\uD83D\uDE02\nGo and Study Bro Exams are near!!\uD83D\uDEAB\uD83E\uDD23");
+}
+function function1(){
+    alert("\u26A0\ufe0fYou are HACKED\u2757\ufe0f\u2757\ufe0f\u26A0\ufe0f\n\u274C That's so Funny!!\uD83D\uDE02");
+}
+function function2(){
+    alert("\u274CAre You Sure!!\uD83E\uDD23");
+}
+function function3(){
+    alert("Noting to Add!!\uD83D\uDC7D");
+}
